bdeac3f77f5f113d58f9d43831ccae7a9aa1d28f
FunsoJoba <hrfunsojoba@gmail.com>
2023-09-01 13:37:35 +0100

    user manager sh

---
+++ assignment.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+
+#Assignment 1
+# Create a Bash script that accepts directory paths as arguments. For each argument, list the contents of each directory with the name of each listed at the top.
+
+# If you want to give yourself an added challenge, 
+# have your script accept an additional argument, 
+# “a” or “d,” that sorts the contents of each directory 
+# in either ascending or descending order. 
+# For even more of a challenge, 
+# create a Bash script that sorts the 
+# contents of the directories based on an “a” or “d” argument that directly precedes it. 
+# In other words, you should be able to sort one directory in ascending order, 
+# a second directory in descending order, and so on.
+
+
+# SOLUTION
+
+
+
+# Assignment 2
+# Another way to work with text in Bash is with AWK commands. 
+# For this challenge we’ll use the following data set of the world’s countries and their capitals.
+
+# Create a Bash script that outputs this dataset into two columns: 
+# one for the country and the other for its capital.
+
+# For an added challenge, 
+# have your script accept a letter as input. 
+# The output should be just the countries that start with the input letter. 
+# For another challenge, create another Bash script that outputs a 
+# two-column list sorted alphabetically by capital city.
+
+#Assignment 3
+# Programmers who work in Bash are often System Administrators who manage computer and server networks. 
+# So, it’s important to know which disks and file systems are mounted — 
+# that is, accessible by the computer’s file system. 
+# Note that just because a disk or 
+# computer is physically attached with cables doesn’t always mean that it’s mounted and accessible.
+
+# Come up with a Bash script that detects whether a given mount point or file system is mounted. 
+# If so, the script should return the amount of used space on the file system and the amount of free space. 
+# If the file system isn’t mounted, your script should return an error message.
+
+# Looking for an extra challenge? 
+# Add a prompt to your script that asks the user if they want to attempt to mount the file system if it’s unmounted. 
+# When the file system mounts, the script should then output the amount of used space and free space in the file system.
+
+
+---
+
+
+# SOLUTION
+
---
+++ user_group.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# create three groups, admin, support & engineering and add the admin group to sudoers.
+# Create a user in each of the groups.
+# Generate SSH keys for the user in the admin group
+
+root_user="root"
+
+groupadd admin
+groupadd support
+groupadd engineering
+
+usermod -G sudo admin
+
+
+useradd admin_user:admin
+useradd support_user:support
+useradd engineering_user:engineering
